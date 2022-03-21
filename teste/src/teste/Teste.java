42ebfea2dfd7cc769aef91b7ed1963fcf03ded74
wesleydiasmaciel <wesleydiasmaciel@gmail.com>
2022-03-21 03:38:26 -0300

    Update Teste.java

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -3,7 +3,9 @@ package teste;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1 = 3, num2 = 5, soma;
+		
+		soma = num1 + num2;
 
 	}
 
